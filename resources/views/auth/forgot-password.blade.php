9a030695f4e7e89cbd24ca100c03f3fc366438b9
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-11-23 16:11:47 -0500

    agregar traducciones, cambiar idioma form login, register, reset email

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? No hay problema. Solo indícanos tu dirección de correo electrónico y te enviaremos un enlace para restablecer tu contraseña que te permitirá elegir una nueva.') }}
     </div>
 
     <!-- Session Status -->
@@ -17,9 +17,14 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+
+            <x-link :enlace="route('login')">
+                Iniciar sesion
+            </x-link>
         </div>
+
+        <x-primary-button class="mt-3 flex justify-center">
+            {{ __('Enviar') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm bg-red-700 text-white font-bold rounded-sm dark:text-red-400 space-y-1 p-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
